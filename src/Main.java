953da63b86f2d7f2a66b4408147cf87a5a444980
blitheguy <103589017+blitheguy@users.noreply.github.com>
2022-04-18 20:16:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        String name="Володя";
+        hello(name);
+    }
+    static void hello(String name){
+        System.out.println("Привет "+name);
+    }
+}
+
